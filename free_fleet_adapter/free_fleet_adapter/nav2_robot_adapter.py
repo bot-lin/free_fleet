d251a39c3059cf5bb4bc64e72c1ab907e885c64c
bot-lin <zlin6364@hotmail.com>
2025-06-20 15:55:15 +0800

    Update nav2_robot_adapter.py

--- free_fleet_adapter/free_fleet_adapter/nav2_robot_adapter.py
+++ free_fleet_adapter/free_fleet_adapter/nav2_robot_adapter.py
@@ -218,10 +218,12 @@ class Nav2RobotAdapter(RobotAdapter):
             )
         )
         if not self.update_handle:
+            # error_message = \
+            #     f'Failed to add robot [{self.name}] to fleet ' \
+            #     f'[{self.fleet_handle.more().fleet_name()}], this is most ' \
+            #     'likely due to a configuration error.'
             error_message = \
-                f'Failed to add robot [{self.name}] to fleet ' \
-                f'[{self.fleet_handle.more().fleet_name()}], this is most ' \
-                'likely due to a configuration error.'
+                f'Failed to add robot [{self.name}] to fleet '
             self.node.get_logger().error(error_message)
             raise RuntimeError(error_message)
 
