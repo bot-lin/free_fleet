618f49a8d1dd9a30ab28396575de5abb687f3b7a
bot-lin <zlin6364@hotmail.com>
2025-06-22 20:12:04 +0800

    Update ros2_types.py

--- free_fleet/free_fleet/ros2_types.py
+++ free_fleet/free_fleet/ros2_types.py
@@ -231,15 +231,15 @@ class SensorMsgs_BatteryState(IdlStruct):
 @dataclass
 class RclInterfaces_ParameterValue(IdlStruct):
     type: uint8
-    bool_value: bool = False
-    integer_value: int64 = 0
-    double_value: float64 = 0.0
-    string_value: str = ''
-    byte_array_value: List[uint8] = []
-    boolean_array_value: List[bool] = []
-    integer_array_value: List[int64] = []
-    double_array_value: List[float64] = []
-    string_array_value: List[str] = []
+    bool_value: bool
+    integer_value: int64
+    double_value: float64
+    string_value: str
+    byte_array_value: List[uint8]
+    boolean_array_value: List[bool]
+    integer_array_value: List[int64]
+    double_array_value: List[float64]
+    string_array_value: List[str]
 @dataclass
 class RclInterfaces_Parameter(IdlStruct):
     name: str
